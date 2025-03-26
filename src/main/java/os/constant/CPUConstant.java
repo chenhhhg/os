5735f460cb471b1c3e991a90c12ffa973f46628d
tfortune <3384824186@qq.com>
2025-03-26 17:31:05 +0800

    a

--- src/main/java/os/constant/CPUConstant.java
+++
@@ -1,6 +0,0 @@
-package os.constant;
-
-public class CPUConstant {
-    public static final int REGISTER_COUNT = 16;
-    public static final int CLOCK_PER_TICK = 5;
-}
--- src/main/java/os/system/SystemTimer.java
+++
@@ -1,8 +0,0 @@
-package os.system;
-
-public class SystemTimer {
-    private long ticks = 0;
-
-    public void tick() { ticks++; }
-    public long getTicks() { return ticks; }
-}
